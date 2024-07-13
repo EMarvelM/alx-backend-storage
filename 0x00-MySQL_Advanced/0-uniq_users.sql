8611401e79968bc49b6905379d6b15aab6c96364
EMarvelM <marvelousegbe19@gmail.com>
2024-07-13 07:17:02 +0100

    fix the syntax error

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- creates a table users following these requirements
-CREATE TABLE IF EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    PRIMARY KEY(id),
+    PRIMARY KEY(id)
 );
