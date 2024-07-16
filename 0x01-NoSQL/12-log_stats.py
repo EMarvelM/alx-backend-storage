e0d06d2a6763a90a9c9d9d57e854734c97946048
EMarvelM <marvelousegbe19@gmail.com>
2024-07-16 12:48:23 +0100

    module should not work when imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,22 +4,26 @@
 
 from pymongo import MongoClient
 
-connection = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = connection.logs.nginx
-logs = nginx_collection.find({})
-print("{} logs\nMethods:".format(nginx_collection.count_documents({})))
+def main():
+    connection = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = connection.logs.nginx
+    logs = nginx_collection.find({})
+    print("{} logs\nMethods:".format(nginx_collection.count_documents({})))
 
-methods = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
-get_satus = 0
+    methods = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
+    get_satus = 0
 
-for log in logs:
-    if log["method"] in methods:
-        if log["method"] == 'GET' and log['path'] == '/status':
-            get_satus += 1
-        methods[log["method"]] += 1
+    for log in logs:
+        if log["method"] in methods:
+            if log["method"] == 'GET' and log['path'] == '/status':
+                get_satus += 1
+            methods[log["method"]] += 1
 
 
-for method in methods:
-    print('    method {}: {:d}'.format(method, methods[method]))
+    for method in methods:
+        print('    method {}: {:d}'.format(method, methods[method]))
 
-print('{} status check'.format(get_satus))
+    print('{} status check'.format(get_satus))
+
+if __name__ == "__main__":
+    main()
