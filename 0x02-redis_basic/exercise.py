b77a8cdf8f17bc7b1c9ee51286928d16ae882027
EMarvelM <marvelousegbe19@gmail.com>
2024-07-19 14:40:33 +0100

    decorate Cache.store with call history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -41,6 +41,7 @@ class Cache:
         self._redis: redis.Redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
